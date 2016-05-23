2504c6b3b6be57212a98b090089217cb2294e97d
Nicholas Hubbard <hubbardn95@gmail.com>
2016-05-23 11:57:07 -0400

    Build

--- release.android.sh
+++ release.android.sh
@@ -4,8 +4,8 @@ ANDROID_ZIPALIGN="/usr/bin/zipalign"
 ANDROID_KEY_PATH="/home/nicholas/.android/main.keystore"
 ANDROID_KEY_ALIAS="alias_name"
 npm run increaseVersion -- -s "major"
-npm run dumpprod
-npm run buildProdAndroid
+# npm run dumpprod
+cordova build android --device --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${ANDROID_KEY_PATH} platforms/android/build/outputs/apk/android-x86-release-unsigned.apk ${ANDROID_KEY_ALIAS}
 jarsigner -verify -certs platforms/android/build/outputs/apk/android-x86-release-unsigned.apk
 ${ANDROID_ZIPALIGN} -vf 4 platforms/android/build/outputs/apk/android-x86-release-unsigned.apk ./build/wphc-android-x86.apk
